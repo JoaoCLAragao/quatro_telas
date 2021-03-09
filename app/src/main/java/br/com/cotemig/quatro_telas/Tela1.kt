52e69eeae9932717f04b16db8c4bd70f4d4ad57f
DESKTOP-I9CN367\Aragao <71600841@aluno.faculdadecotemig.br>
2021-03-08 22:22:12 -0300

    Fim da montagem da primeira tela.

---
+++ app/src/main/java/br/com/cotemig/quatro_telas/Tela1.kt
@@ -0,0 +1,11 @@
+package br.com.cotemig.quatro_telas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tela1)
+    }
+}
