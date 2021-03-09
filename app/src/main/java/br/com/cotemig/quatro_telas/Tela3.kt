effaa21ad1edfb2c8f104f9b78a6c8f4908d8f62
DESKTOP-I9CN367\Aragao <71600841@aluno.faculdadecotemig.br>
2021-03-09 02:16:08 -0300

    Commit da quarta e última tela.

--- app/src/main/java/br/com/cotemig/quatro_telas/Tela3.kt
+++ app/src/main/java/br/com/cotemig/quatro_telas/Tela3.kt
@@ -3,9 +3,9 @@ package br.com.cotemig.quatro_telas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class tela2 : AppCompatActivity() {
+class Tela3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tela2)
+        setContentView(R.layout.tela3)
     }
 }
